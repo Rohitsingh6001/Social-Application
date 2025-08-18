59031b9214f7b8201f616bddd069ddc77edc5f33
Rohitsingh6001 <Rohitsingh837000@gmail.com>
2025-08-18 18:51:21 +0530

    Ok completed

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {assets} from '../assets/assets.js'
+
+const Login = () => {
+  return (
+    <div className='min-h-screen flex flex-col md:flex-row'>
+     {/* {Background Image}  */}
+      <img src={assets.bgImage} alt='' className='absolute top-0 left-0 -z-1 w-full h-full object-cover'/>
+
+      {/* left side : Branding  */}
+
+      <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
+          <img src={assets.logo} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
