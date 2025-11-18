0ba520aeedf11efd7cae8e33a1c3e627b170204b
Rohitsingh6001 <Rohitsingh837000@gmail.com>
2025-11-18 15:00:39 +0530

    Ok completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,17 @@ import Connections from './pages/Connections';
 import Discover from './pages/Discover';
 import Profile from './pages/Profile';
 import CreatePost from './pages/CreatePost';
+import {useUser} from '@clerk/clerk-react'
+import Layout from './pages/Layout.jsx'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
+  const {user} = useUser()
   return (
     <>
+    <Toaster/>
        <Routes>
-            <Route path='/'  element={<Login/>}>
+            <Route path='/'  element={ !user ? <Login/> : <Layout/>}>
                <Route index element={<Feed/>}/>
                <Route path='messages' element={<Messages/>}/>
                <Route path='messages/:userId' element={<ChatBox/>}/>
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { dummyPostsData } from "../assets/assets";
+import { Loading } from "../components/Loading.jsx";
+import StoriesBar from "../components/StoriesBar.jsx";
+import PostCard from "../components/PostCard.jsx";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [feeds, setfeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default Feed
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+      {/* { Stories and post list } */}
+      <div>
+        <StoriesBar/>
+        <div className="p-4 space-y-6">{feeds.map((post) =>(
+          <PostCard key={post._id}  post={post}/>
+        ))}</div>
+      </div>
+      {/* {Right sidebar } */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+
+        </div>
+        <h1>Recent message</h1>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Feed;
